f7a5eea155b6566be7b8df3d7e4b0dc4430d9347
richard <richard.zsupos@gmail.com>
2024-02-21 22:02:11 +0100

    format 436

--- 436.ts
+++ 436.ts
@@ -4,28 +4,28 @@ function findRightInterval(intervals: number[][]): number[] {
     let startIntervals: number[] = [];
     let startIndexes: number[] = [];
 
-    for(let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         startIntervals.push(intervals[i][0]);
         startIndexes[intervals[i][0]] = i;
         result[i] = -1;
     }
 
-    startIntervals.sort((a,b) => a - b);
+    startIntervals.sort((a, b) => a - b);
 
-    for(let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         let left = 0;
         let right = n - 1;
 
         let target = intervals[i][1];
         let currentIntervalStartIndex = startIndexes[intervals[i][0]];
 
-        while(left <= right){
-            let mid = Math.floor((left + right)/2);
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
 
-            if(startIntervals[mid] >= target){
+            if (startIntervals[mid] >= target) {
                 result[currentIntervalStartIndex] = startIndexes[startIntervals[mid]];
                 right = mid - 1;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
