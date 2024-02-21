d463e6b84bb3b9744564111c6872fbe7b9b1bf14
richard <richard.zsupos@gmail.com>
2024-02-21 22:01:38 +0100

    Find Right Interval

---
+++ 436.ts
@@ -0,0 +1,35 @@
+function findRightInterval(intervals: number[][]): number[] {
+    let result: number[] = [];
+    let n = intervals.length;
+    let startIntervals: number[] = [];
+    let startIndexes: number[] = [];
+
+    for(let i = 0; i < n; i++){
+        startIntervals.push(intervals[i][0]);
+        startIndexes[intervals[i][0]] = i;
+        result[i] = -1;
+    }
+
+    startIntervals.sort((a,b) => a - b);
+
+    for(let i = 0; i < n; i++){
+        let left = 0;
+        let right = n - 1;
+
+        let target = intervals[i][1];
+        let currentIntervalStartIndex = startIndexes[intervals[i][0]];
+
+        while(left <= right){
+            let mid = Math.floor((left + right)/2);
+
+            if(startIntervals[mid] >= target){
+                result[currentIntervalStartIndex] = startIndexes[startIntervals[mid]];
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+    }
+
+    return result;
+};
