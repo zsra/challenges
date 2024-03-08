c39cac562fc16144269f55fe6f1ded0b12e0dd92
richard <richard.zsupos@gmail.com>
2024-03-08 20:41:47 +0100

    Binary Tree Level Order Traversal

---
+++ TOP100/102.java
@@ -0,0 +1,26 @@
+class Solution {
+
+  List<List<Integer>> levels = new ArrayList<>();
+
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    if (root == null) {
+      return levels;
+    }
+    helper(root, 0);
+    return levels;
+  }
+
+  public void helper(TreeNode node, int level) {
+    if (levels.size() == level) {
+      levels.add(new ArrayList<Integer>());
+    }
+    levels.get(level).add(node.val);
+
+    if (node.left != null) {
+      helper(node.left, level + 1);
+    }
+    if (node.right != null) {
+      helper(node.right, level + 1);
+    }
+  }
+}
