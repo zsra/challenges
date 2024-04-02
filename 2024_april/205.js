d9845cd3c719d2c005b0fb3f94836585d205d0ba
richard <richard.zsupos@gmail.com>
2024-04-02 17:44:32 +0200

    Isomorphic Strings

---
+++ 2024_april/205.js
@@ -0,0 +1,8 @@
+var isIsomorphic = function (s, t) {
+
+    if (s.length !== t.length) return false;
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) !== t.indexOf(t[i])) return false;
+    }
+    return true;
+};
