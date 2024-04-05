91b3ca4543f866596102e0fb51c84d47bd13cd8b
richard <richard.zsupos@gmail.com>
2024-04-05 12:56:21 +0200

    Make The String Great

---
+++ 2024_april/1544.js
@@ -0,0 +1,10 @@
+var makeGood = function (s) {
+    const stack = [''];
+
+    for (let c of s) {
+        let top = stack[stack.length - 1];
+        if (top.toLowerCase() === c.toLowerCase() && top !== c) stack.pop()
+        else stack.push(c);
+    }
+    return stack.join('');
+};
