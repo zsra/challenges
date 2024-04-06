2fb972e64966bd05a64372b7969ed144837238ed
richard <richard.zsupos@gmail.com>
2024-04-06 19:26:27 +0200

    Minimum Remove to Make Valid Parentheses

---
+++ 2024_april/1249.js
@@ -0,0 +1,13 @@
+var minRemoveToMakeValid = function (s) {
+    s = s.split("")
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "(") stack.push(i)
+        else if (s[i] == ")") {
+            if (stack.length) stack.pop()
+            else s[i] = ""
+        }
+    }
+    for (let i of stack) s[i] = ""
+    return s.join("")
+};
