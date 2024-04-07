bcd29e46b0e533c28e356568b03e43399f5bb1ce
richard <richard.zsupos@gmail.com>
2024-04-07 13:39:37 +0200

    Valid Parenthesis String

---
+++ 2024_april/678.js
@@ -0,0 +1,21 @@
+var checkValidString = function (s) {
+    let leftMin = 0;
+    let leftMax = 0;
+
+    for (let c of s) {
+        if (c === '(') {
+            leftMin++;
+            leftMax++;
+        } else if (c === ')') {
+            leftMin--;
+            leftMax--;
+        } else {
+            leftMin--;
+            leftMax++;
+        }
+        if (leftMax < 0) return false;
+        if (leftMin < 0) leftMin = 0;
+    }
+
+    return leftMin === 0;
+};
