4046240a6faf0749d72b422062cc3dc9c49d018e
richard <richard.zsupos@gmail.com>
2024-04-09 23:20:12 +0200

    Students and Examinations

---
+++ SQL50/1280.sql
@@ -0,0 +1,11 @@
+SELECT 
+    s.student_id,
+    s.student_name,
+    b.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects b
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND b.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, b.subject_name
+ORDER BY s.student_id, b.subject_name
