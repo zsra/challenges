2b39ac438ee9bb66cda2c23a8c421d4bf2e37d17
richard <richard.zsupos@gmail.com>
2024-04-10 13:04:11 +0200

    Reveal Cards In Increasing Order

---
+++ 2024_april/950.js
@@ -0,0 +1,21 @@
+var deckRevealedIncreasing = function(deck) {
+    let revealedDeck = [];
+    let queue = [];
+
+    deck.sort(function (a, b) {  return a - b;  });
+
+    for(let index = 0; index < deck.length; index++) {
+        queue.push(index);
+    }
+
+    for(let index = 0; index < deck.length; index++) {
+        revealedDeck[queue[0]] = deck[index];
+        queue.shift();
+        queue.push(queue[0]);
+        queue.shift();
+    }
+
+    return revealedDeck;
+};
+
+deckRevealedIncreasing([17,13,11,2,3,5,7]);
