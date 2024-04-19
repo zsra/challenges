79b741276285a0d6446139307ca6f5fec390de0f
richard <richard.zsupos@gmail.com>
2024-04-19 23:50:27 +0200

    FIX capital in 1211.sql

--- SQL50/1211.sql
+++ SQL50/1211.sql
@@ -1,7 +1,7 @@
 SELECT 
 query_name, 
-round(avg(rating/position),2) as quality, 
-round(sum(rating < 3)/count(*)*100,2) as poor_query_percentage
+ROUND(AVG(rating/position),2) AS quality, 
+ROUND(SUM(rating < 3)/COUNT(*)*100,2) AS poor_query_percentage
 FROM Queries
 WHERE query_name IS NOT NULL
 GROUP BY query_name
