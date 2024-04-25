c836c6faa18be46e2abf78b14d8481c5af1dcfaf
richard <richard.zsupos@gmail.com>
2024-04-25 21:55:20 +0200

    Friend Requests II: Who Has the Most Friends

--- SQL50/602.sql
+++ SQL50/602.sql
@@ -1,3 +1,11 @@
-SELECT * FROM Cinema
-WHERE MOD(id, 2) = 1 AND description != "boring"
-ORDER BY rating DESC
+SELECT id, COUNT(id) AS num
+FROM (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL 
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) t1
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
