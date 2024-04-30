0a30f82f1c1f1acd711d47df31299119c839c6aa
richard <richard.zsupos@gmail.com>
2024-04-30 09:44:02 +0200

    Number of Wonderful Substrings

---
+++ 2024_april/1915.js
@@ -0,0 +1,24 @@
+var wonderfulSubstrings = function (word) {
+  const cnts = new Array(1024).fill(0)
+
+  cnts[0] = 1
+
+  let mask = 0
+  let ans = 0
+
+  for (let i = 0; i < word.length; i++) {
+    const k = word[i].charCodeAt(0) - 97
+
+    mask ^= 1 << k
+    ans += cnts[mask]
+
+    for (let j = 0; j < 10; j++) {
+      const code = mask ^ (1 << j)
+      ans += cnts[code]
+    }
+
+    cnts[mask] += 1
+  }
+
+  return ans
+}
