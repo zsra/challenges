66d1f4c798154c4b665ee881ae1bbe9129b8935e
richard <richard.zsupos@gmail.com>
2024-05-06 13:51:29 +0200

    Remove Nodes From Linked List

---
+++ 2024.may/2487.cs
@@ -0,0 +1,12 @@
+public ListNode RemoveNodes(ListNode head) {
+    if(head.next == null)
+        return head;
+
+    var temp = RemoveNodes(head.next);
+    if(head.val >= temp.val) {
+        head.next = temp;
+        return head;
+    }
+
+    return temp;  
+}
