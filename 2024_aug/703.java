bf3b56105faac2036e7868e4d207071599fe64a7
Richard Zsupos <richard.zsupos@gmail.com>
2024-08-12 17:09:15 +0200

    Kth Largest Element in a Stream

---
+++ 2024_aug/703.java
@@ -0,0 +1,24 @@
+class KthLargest {
+
+    PriorityQueue<Integer> minHeap;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        minHeap = new PriorityQueue<>();
+        this.k = k;
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k || minHeap.peek() < val) {
+            minHeap.add(val);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
+        }
+        return minHeap.peek();
+    }
+}
