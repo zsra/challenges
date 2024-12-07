505cb378786fe1cd7e6f82cd34c40f9d62287134
Richard Zsupos <richard.zsupos@gmail.com>
2024-12-07 08:41:48 +0100

    Minimum Limit of Balls in a Bag

---
+++ 2024_dec/1760.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MinimumSize(int[] nums, int maxOperations)
+    {
+        int left = 1, right = nums.Max();
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2, count = 0;
+            foreach (int a in nums) count += (a - 1) / mid;
+            if (count > maxOperations)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+	}
